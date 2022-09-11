04b7b7b7eb6ab18ef22d01334bfffcebe9369377
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2022-09-11 21:02:32 +0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,11 +5,4 @@ function cal (x){
  result = result + x;
 
 document.getElementById("display_x").innerHTML = result;
-
-}
-function ac (x){
-var aresult = 0; document.getElementById("display_x").innerHTML = aresult;}
-function equ (x){
-var fresult = result; document.getElementById("display_x").innerHTML = fresult;
-
 }
